55855d23f6ae60c7dd33b8c403d736d8d2f3fe81
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-28 12:31:45 -0300

    feat: Config route to receive file

--- backend/src/modules/upload/upload.controller.ts
+++ backend/src/modules/upload/upload.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+
+  @Post('/')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+  }
 }
