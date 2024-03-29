5868a5fad83553f5e8a155375e5e54d014678212
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 17:18:15 -0300

    refactor: Add icon for dashboard

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import IconChart from './icons/IconChart.vue'
 import IconDownload from './icons/IconDownload.vue'
 import IconHome from './icons/IconHome.vue'
+import IconSettings from './icons/IconSettings.vue'
 import IconUpload from './icons/IconUpload.vue'
 </script>
 
@@ -9,21 +11,14 @@ import IconUpload from './icons/IconUpload.vue'
     <div class="h-[80px] bg-gray-900 flex justify-center items-center">
       <h3 class="font-bold text-xl">Business<span class="text-green">.B</span>ase</h3>
     </div>
-    <div class="h-[calc(100vh-80px)] bg-gray-800 py-[20px]">
-      <div class="px-[20px] flex flex-col">
+    <div class="flex flex-col justify-between h-[calc(100vh-80px)] bg-gray-800 py-[20px]">
+      <div class="px-[20px] flex flex-col gap-4">
         <router-link
           to="/home"
           class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out"
         >
-          <IconHome />
-          Home
-        </router-link>
-        <router-link
-          to="/download"
-          class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300 hover:text-gray-800 transition duration-400 ease-in-out"
-        >
-          <IconDownload />
-          Download
+          <IconChart />
+          Dashboard
         </router-link>
         <router-link
           to="/download"
@@ -31,6 +26,16 @@ import IconUpload from './icons/IconUpload.vue'
         >
           <IconUpload />
           Upload
+        </router-link>
+      </div>
+
+      <div class="px-[20px] flex flex-col">
+        <router-link
+          to="/home"
+          class="flex items-center gap-2 py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out"
+        >
+          <IconSettings />
+          Configurações
         </router-link>
       </div>
     </div>
---
+++ frontend/src/components/icons/IconChart.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    aria-hidden="true"
+    fill="currentColor"
+    class="mr-2 w-[25px] h-[25px] fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    height="14"
+    width="12.25"
+    viewBox="0 0 448 512"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M160 80c0-26.5 21.5-48 48-48h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V80zM0 272c0-26.5 21.5-48 48-48H80c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V272zM368 96h32c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48H368c-26.5 0-48-21.5-48-48V144c0-26.5 21.5-48 48-48z"
+    />
+  </svg>
+</template>
