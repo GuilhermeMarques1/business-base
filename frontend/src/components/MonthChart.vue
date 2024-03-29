a0355231421275e0478ca66b3ecdbc072a327fcd
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 11:42:51 -0300

    feat: Create chart

---
+++ frontend/src/components/MonthChart.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Chart from 'chart.js/auto'
+import { onMounted } from 'vue'
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June']
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'My first dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }
+  ]
+}
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {}
+}
+
+onMounted(() => {
+  const myChart = new Chart(document.getElementById('myChart'), config)
+})
+</script>
+
+<template>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
+</template>
