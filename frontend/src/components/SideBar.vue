9678f8a01a8be8831d86f54dc7564a29cdbeff6c
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 01:56:37 -0300

    feat: Add sidebar

---
+++ frontend/src/components/SideBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="w-[400px] h-full bg-gray-200 text-white">
+    <div class="h-[50px] bg-gray-900 flex justify-center items-center">
+      <h3 class="font-bold text-xl">Admin Dashboard</h3>
+    </div>
+    <div class="h-[calc(100vh-50px)] bg-gray-800 py-[20px]">
+      <div class="px-[20px] flex flex-col">
+        <router-link
+          to="/home"
+          class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md border-gray-200 hover:bg-gray-200 hover:text-gray-800 transition duration-400 ease-in-out"
+        >
+          <svg
+            aria-hidden="true"
+            class="mr-2 w-[25px] h-[25px] fill-current"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          Home
+        </router-link>
+        <router-link
+          to="/download"
+          class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300 hover:text-gray-800 transition duration-400 ease-in-out"
+        >
+          <svg
+            aria-hidden="true"
+            class="mr-2 w-[25px] h-[25px] fill-current"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          Download
+        </router-link>
+        <router-link
+          to="/download"
+          class="inline-flex relative items-center py-[10px] px-[10px] w-full text-sm font-medium rounded-md rounded-b-lg hover:bg-gray-300 hover:text-gray-800 transition duration-400 ease-in-out"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512">
+            <path
+              fill="#ffffff"
+              d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"
+            />
+          </svg>
+          Upload
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
