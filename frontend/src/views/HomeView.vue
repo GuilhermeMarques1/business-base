153279db09468f763050c6a580c7e5f53f5cad97
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 16:47:10 -0300

    feat: Add icon to admin profile

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -18,10 +18,24 @@ const handleClick = () => {
 <template>
   <div class="w-screen h-screen flex flex-col">
     <header
-      class="h-[80px] bg-gray-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b"
+      class="h-[80px] bg-gray-100 flex items-center justify-between shadow-sm px-[20px] w-full py-[10px] z-10 border-b"
     >
       <div class="cursor-pointer w-[30px]" @click="handleClick">
         <IconHamburguer />
+      </div>
+
+      <div class="w-[300px]">
+        <div class="flex items-center justify-start space-x-4">
+          <img
+            class="w-12 h-12 rounded-full border-2 border-gray-50"
+            src="https://avatars.githubusercontent.com/u/81578826?s=400&u=97c14641f113422c1670984df587fb98df99ee69&v=4"
+            alt=""
+          />
+          <div class="font-semibold dark:text-black text-left">
+            <div>Guilherme Marques</div>
+            <div class="text-xs text-gray-500 dark:text-gray-400">Admin</div>
+          </div>
+        </div>
       </div>
     </header>
 
