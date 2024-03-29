3ff87c349d78fe7d7cbf541c39ceee1a309194ab
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 18:42:25 -0300

    feat: Add upload view

---
+++ frontend/src/components/UploadFile.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="border border-dashed border-gray-400 rounded-lg p-14 mb-14">
+    <div class="flex items-center justify-center mb-4">
+      <label for="fileInput" class="cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-12 w-12 text-gray-500"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 12a1 1 0 0 1-1-1V3H7a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-8a1 1 0 0 1-1-1z"
+            clip-rule="evenodd"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M15 10a1 1 0 0 0-1-1H6a1 1 0 0 0 0 2h8a1 1 0 0 0 1-1z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </label>
+      <input
+        type="file"
+        id="fileInput"
+        class="hidden"
+        accept=".csv,.xlsx"
+        @change="handleFileUpload"
+      />
+      <span class="ml-2">Clique ou arraste arquivos aqui</span>
+    </div>
+    <div class="text-sm text-gray-500 mb-4">
+      Tamanho máximo: 10MB. Tipos de arquivo suportados: CSV, XLSX
+    </div>
+    <div class="text-gray-500">
+      Faça o upload do seu arquivo para análise de métricas do seu negócio
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleFileUpload(event) {
+      const file = event.target.files[0]
+      console.log('Arquivo selecionado:', file)
+    }
+  }
+}
+</script>
---
+++ frontend/src/views/UploadView.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import UploadFile from '@/components/UploadFile.vue'
+import IconHamburguer from '@/components/icons/IconHamburguer.vue'
+import { inject } from 'vue'
+
+const toggleSideBar = inject<() => void>('toggleSideBar')
+</script>
+
+<template>
+  <div class="w-screen h-screen flex flex-col">
+    <header
+      class="h-[80px] bg-gray-100 flex items-center justify-between shadow-sm px-[20px] w-full py-[10px] z-10 border-b"
+    >
+      <div class="cursor-pointer w-[30px]" @click="toggleSideBar">
+        <IconHamburguer />
+      </div>
+
+      <div class="w-[300px]">
+        <div class="flex items-center justify-start space-x-4">
+          <img
+            class="w-12 h-12 rounded-full border-2 border-gray-50"
+            src="https://avatars.githubusercontent.com/u/81578826?s=400&u=97c14641f113422c1670984df587fb98df99ee69&v=4"
+            alt=""
+          />
+          <div class="font-semibold dark:text-black text-left">
+            <div>Guilherme Marques</div>
+            <div class="text-xs text-gray-500 dark:text-gray-400">Admin</div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <main class="flex flex-col items-center">
+      <div class="h-[calc(100vh-80px)] flex items-center justify-center">
+        <UploadFile />
+      </div>
+    </main>
+  </div>
+</template>
