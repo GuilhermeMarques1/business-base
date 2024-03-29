0c274b5cbd0ee77f5d4ccc7f6ec92cd09d564db8
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 15:54:09 -0300

    feat: Add pie chart

---
+++ frontend/src/components/TopClients.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Chart, { type ChartItem } from 'chart.js/auto'
+import { onMounted } from 'vue'
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow'],
+  datasets: [
+    {
+      label: 'My First Dataset',
+      data: [300, 50, 100],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+      hoverOffset: 4
+    }
+  ]
+}
+
+const config = {
+  type: 'doughnut',
+  data: data
+}
+
+onMounted(() => {
+  const element = document.getElementById('topClients') as ChartItem
+  new Chart(element, config)
+})
+</script>
+
+<template>
+  <div>
+    <canvas id="topClients"></canvas>
+  </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import MonthChart from '@/components/MonthChart.vue'
 import IconHamburguer from '@/components/icons/IconHamburguer.vue'
+import BoxIndicator from '@/components/BoxIndicator.vue'
+import TopClients from '@/components/TopClients.vue'
 
 const props = defineProps<{
   toggleSideBar?: Function
@@ -16,15 +18,23 @@ const handleClick = () => {
 <template>
   <div class="w-screen h-screen flex flex-col">
     <header
-      class="h-[50px] bg-gray-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b"
+      class="h-[80px] bg-gray-100 flex items-center shadow-sm px-[20px] w-full py-[10px] z-10 border-b"
     >
       <div class="cursor-pointer w-[30px]" @click="handleClick">
         <IconHamburguer />
       </div>
     </header>
 
-    <main>
-      <MonthChart />
+    <main class="flex flex-col items-center">
+      <BoxIndicator />
+      <div class="flex flex-row gap-20 h-[500px] w-[80%] mt-14">
+        <div class="h-[500px] w-[80%]">
+          <MonthChart />
+        </div>
+        <div class="h-[200px] w-[30%]">
+          <TopClients />
+        </div>
+      </div>
     </main>
   </div>
 </template>
