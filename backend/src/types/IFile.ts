ff735cc68287ac254c3bb31226fe2742929c1ace
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-29 22:52:27 -0300

    dev: Create interface for file

---
+++ backend/src/types/IFile.ts
@@ -0,0 +1,11 @@
+export interface IFile {
+  'quantidade cobranças': any;
+  'cobrada a cada X dias': any;
+  'data início': any;
+  status: string;
+  'data status': any;
+  'data cancelamento': string;
+  valor: any;
+  'próximo ciclo': string;
+  'ID assinante': string;
+}
